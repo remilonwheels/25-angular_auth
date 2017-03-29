7f9203ef578eaf268fcfebd9f3e24d631d3a344f
Remil Marzan <remilonwheels@gmail.com>
2017-03-28 17:42:43 -0700

    css

--- lab-remil/webpack.config.js
+++ lab-remil/webpack.config.js
@@ -6,7 +6,6 @@ const webpack = require('webpack');
 const HTMLPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-
 module.exports = {
   devtool: 'eval',
   entry: `${__dirname}/app/entry.js`,
