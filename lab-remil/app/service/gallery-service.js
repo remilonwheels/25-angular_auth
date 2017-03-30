5220c2581c43854dc4b44bcb2247dfd47f5e2a46
Remil Marzan <remilonwheels@gmail.com>
2017-03-30 14:15:29 -0700

    completed functionality and css for gallery item

--- lab-remil/app/service/gallery-service.js
+++ lab-remil/app/service/gallery-service.js
@@ -79,5 +79,67 @@ function galleryService($q, $log, $http, authService) {
     });
   };
 
+  service.updateGallery = function(galleryID, galleryData) {
+    $log.debug('galleryService.updateGallery');
+
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/gallery/${galleryID}`;
+      let config = {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      };
+
+      return $http.put(url, galleryData, config);
+    })
+    .then( res => {
+      for (let i=0; i < service.galleries.length; i++) {
+        let current = service.galleries[i];
+        if (current._id === galleryID) {
+          service.galleries[i] = res.data;
+          break;
+        }
+      }
+
+      return res.data;
+    })
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
+  service.deleteGallery = function(galleryID) {
+    $log.debug('galleryService.deleteGallery');
+
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/gallery/${galleryID}`;
+      let config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      return $http.delete(url, config);
+    })
+    .then( res => {
+      for (let i=0; i < service.galleries.length; i++) {
+        let current = service.galleries[i];
+        if (current._id === galleryID) {
+          service.galleries.splice(i, 1);
+          break;
+        }
+      }
+    })
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
   return service;
 }
